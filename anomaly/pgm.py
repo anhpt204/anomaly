d0f56092fb5aacecc10cc2303112d7b0ea4b2ee0
anhpt204 <anh.pt204@gmail.com>
2015-10-26 23:37:29 +0700

    result

--- anomaly/pgm.py
+++ anomaly/pgm.py
@@ -13,6 +13,11 @@ from libpgm.pgmlearner import PGMLearner
 
 from BNfinder.BDE import BDE
 from BNfinder.data import dataset
+
+from sklearn import mixture
+import numpy as np
+import math
+from operator import itemgetter
  
 
 data='data/20150531.txt'
@@ -101,9 +106,105 @@ def anomaly_libpgm():
         
         print result.E
         
+def pdf(network_file, points):
+    '''
+    @param network_file: a bayesian network file that output from BNfinder 
+    @param points: list of points, 
+    each point in the form of a dict {'name':name, 'm_received':v1, 'm_spent':v2, 'i_bought':v3, 'i_sold':v4} 
+    '''
+    #load network
+    bn = eval(open(network_file).read())
+    
+    nodes = [name for name in points[0].keys() if name != 'name']
+    model={}    
+    parents = {}
+    #load model
+    for node in nodes:
+        # if node have no parents
+        states = [()]
+        model[node]=[]
+        
+        print bn[node]['pars']
+
+        if len(bn[node]['pars']) > 0:
+            states=[(0,), (1,)]
             
+            
+        for state in states:
+#             print bn[node]['cpds'][state]
+#             print bn[node]['floatParams']
+                    
+            params = eval(bn[node]['floatParams'])
+#             print params[2]
+                        
+            gmm=mixture.GMM(n_components=2)
+            
+            gmm.weights_=np.array([[bn[node]['cpds'][state][0]], [bn[node]['cpds'][state][1]]])
+            gmm.means_=np.array([[params[0]], [params[1]]])
+            gmm.covars_= np.array([[params[2]],[params[2]]]) ** 2 
+            
+            model[node].append(gmm)
+            
+    # calculate pdf for each point
+    cal_order=['m_received', 'i_bought', 'm_spent', 'i_sold']
+    
+    result=[]
+    for point in points:
+        node_state = {}
+        log_likelihood=0
+        
+        for node in cal_order:
+            gauss_idx=0
+            if len(model[node]) > 1:
+                parent_node = bn[node]['pars'][0]
+                
+                gauss_idx = node_state[parent_node]
+                
+            gauss = model[node][gauss_idx]
+            
+#             probs_gauss = gauss.predict_proba([point[node]])
+            probs = gauss.score([point[node]])
+            
+            if probs[0] > probs[1]:
+                node_state[node]=0
+                log_likelihood += probs[0]
+            else:
+                node_state[node]=1
+                log_likelihood += probs[1]
+        
+        result.append((point['name'], log_likelihood, point))
+        
+    result = sorted(result, key=itemgetter(1))
+    
+    num_of_outliers = int(0.01 * len(result))
+    
+    print num_of_outliers
+    
+    print result[:num_of_outliers]
+    users = [str(usr[0])+'\n' for usr in result[:num_of_outliers]]
+    open(basename(network_file)+'.out', 'w').writelines(users)
+            
+def get_data_points(data_file):
+    lines = open(data_file).readlines()
+    points = []
+    for line in lines[1:]:
+        
+        xs = line.split()
+        
+        point = {}
+        point['name']=xs[0]
+        vs = [float(x) for x in xs[2:]]
+        point['m_spent'], point['m_received'], point['i_sold'], point['i_bought'] = vs
+        
+        points.append(point)
+        
+    return points
+        
 if __name__ == '__main__':
-    prepare_data_bnfinder()
+#     prepare_data_bnfinder()
+#     vars = ['i_sold', 'm_spent', 'i_bought', 'm_received']
+    points = get_data_points('data/20150629.txt')
+    pdf('data/20150629.cpd', points)
 
 #     anomaly_libpgm()
     
